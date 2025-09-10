b66c8eca21bff6960512e7286f134d76d2f95ed5
centroculturaldoibura-dev <centroculturaldoibura@gmail.com>
2025-09-10 08:00:18 -0300

    Update start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+set -e
 
 # Rodar migrações
 python manage.py migrate --noinput
 
 # Criar usuário admin se não existir
-python csi_erp/init_admin.py || true
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'Admin123')
+"
 
 # (Opcional) coletar estáticos
-python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Admin123')"
-
+python manage.py collectstatic --noinput || true
 
 # Subir o servidor
 python manage.py runserver 0.0.0.0:8000
